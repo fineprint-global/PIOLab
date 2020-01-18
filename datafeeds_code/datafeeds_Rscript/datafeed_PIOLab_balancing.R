6ab344095957f3da1d9375021b6748fff2b6fde5
Hanspeter85 <hanspeter.wieland@wu.ac.at>
2020-01-18 11:34:14 +1100

    ...

--- datafeeds_code/datafeeds_Rscript/datafeed_PIOLab_balancing.R
+++ datafeeds_code/datafeeds_Rscript/datafeed_PIOLab_balancing.R
@@ -81,7 +81,7 @@ dir.create(path_set)
 # Write data frame with ALANG commands as tab-delimited txt-file to root and working directory (mother)
 # Note HP: This is probably not the normal procedure, meaning no IE ALANG's in the root
 filename <-  paste0(path_set,"/",gsub("-","",Sys.Date()),
-                      "_PIOLab_Balancing_000_Constraints-",year,"_000_RoWincluded.txt")
+                      "_PIOLab_Balancing_000_Constraints-",year,"_000_RoWexcluded.txt")
   
 write.table(ALANG,file = filename,row.names = FALSE, quote = F,sep = "\t")
   
