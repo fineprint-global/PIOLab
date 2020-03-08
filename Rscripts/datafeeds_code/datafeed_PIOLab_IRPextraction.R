bbef164d9c2a87cc3c1d20fe22f1222503307a8f
Hanspeter85 <hanspeter.wieland@wu.ac.at>
2020-03-08 22:45:55 +1100

    IRP extraction data feed works perfectly.

--- Rscripts/datafeeds_code/datafeed_PIOLab_IRPextraction.R
+++ Rscripts/datafeeds_code/datafeed_PIOLab_IRPextraction.R
@@ -3,6 +3,12 @@
 datafeed_name <- "IRPextraction"
 print(paste0("datafeed_PIOLab_",datafeed_name," initiated."))
 
+# Create wrapper for writing clean data 
+WriteFile <- function(d,t)
+{
+  write.table(d,file = t,row.names = FALSE,col.names = FALSE,sep = ",",na = "NaN")
+}
+
 ################################################################################
 # Set library path when running on suphys server
 if(Sys.info()[1] == "Linux"){
@@ -19,8 +25,20 @@ source(paste0(path$Subroutines,"/Read_ExtractionIRP.R"))
 
 # Loading function for estimating SE with linear regression
 source(paste0(path$Subroutines,"/SE_LogRegression.R"))
+
 RSE <- filter(read.xlsx(path$RSE_settings),Item == datafeed_name)
+
 data <- SE_LogRegression(data,RSE$Minimum,RSE$Maximum)
+
+n_reg <- nrow(root$region) # Number of root regions
+
+data_new <- matrix(NaN,nrow = n_reg,ncol = 2)
+
+data_new[data$Code,1] <- data$Quantity
+data_new[data$Code,2] <- data$SE
+data <- as.data.frame(data_new)
+colnames(data) <- c("RHS","SE")
+remove(data_new)
 
 # Create empty ALANG table with header
 source(paste0(path$Subroutines,"/makeALANGheadline.R"))
@@ -30,95 +48,61 @@ path_set <- paste0(path$root,"ProcessedData/",datafeed_name)
 if(dir.exists(path_set)) unlink(path_set,recursive = TRUE) 
 dir.create(path_set)
 
+# Set path to Sector Concordance
+filename <- list("IndAgg" ="Sector Aggregators/20IndV1_SectorAggregatorIndustries.csv")
 
-################################################################################
-# Add lines for regions with non-zero data
+IndAgg <- read.csv(paste0(path$Concordance,"/",filename$IndAgg),header = FALSE) 
+IndAgg <- as.matrix(IndAgg)
 
-for(i in 1:nrow(data))
-{ 
-  # Get root_code of region 
-  reg_num <- data$Code[i]
-  reg_name <- as.character(root$region$Name[reg_num])
-  reg_num <- as.character(reg_num)
+# Create Root2Root region concordance (identity )
+RegAgg <- diag(length(root$region$Code)) # Create region aggregator
+
+# Set name of concordance
+filename["RegAgg"] <- "Root2Root_Reg_Concordance.csv"
+
+# Write to folder 
+WriteFile(RegAgg,paste0(path$Concordance,"/",filename$RegAgg))
+
+# Define section of path to processed data file
+filename["RHS"] <- paste0(path$Processed,"/",datafeed_name,"/",year,"_")
+
+for(i in root$region$Code) 
+{
+  data_sel <- data.frame("RSE" = rep(NaN,n_reg),"SE" = rep(NaN,n_reg))
   
-  # Write data to processed folder
-  # First, value RHS
-  export_value <- matrix(c(data$Quantity[i],0),nrow = 2,ncol = 1)
-  filename_value <- paste0(datafeed_name,"_Value_",year,"_",reg_name,".csv")
-  write.table(export_value,row.names = FALSE,col.names = FALSE,sep = ",",
-              file = paste0(path_set,"/",filename_value))  
+  data_sel$RSE[i] <- data$RHS[i]
+  data_sel$SE[i] <- data$SE[i]
   
-  # Second, standard errors
-  export_SE <- matrix(c(data$SE[i],0),nrow = 2,ncol = 1)
-  filename_SE <- paste0(datafeed_name,"_SE_",year,"_",reg_name,".csv")
-  write.table(export_SE,row.names = FALSE,col.names = FALSE,sep = ",",
-              file = paste0(path_set,"/",filename_SE))  
-  
-  # Read extraction value
-  value <- paste0("DATAPATH/",datafeed_name,"/",filename_value)
-  # Set SE
-  SE <-  paste0("DATAPATH/",datafeed_name,"/",filename_SE) 
-  
-  # Add command for domestic Use table
-  ALANG <- add_row(ALANG,'1' = paste0("DataFeed IRP Extraction ",reg_name),
-                   Value = value,'Row parent' = reg_num,'Column parent' = reg_num,S.E. = SE)
-}
-
-################################################################################
-# Add zero for all other regions
-
-AllOtherRegions <- setdiff(root$region$Code,data$Code)
-
-export <- matrix(0,nrow = 2,ncol = 1) # set values
-filename <- paste0(datafeed_name,"_Value&SE_",year,"_AllOtherRegions.csv")
-SE <- value <- paste0("DATAPATH/",datafeed_name,"/",filename)
-
-# Write values/SE to file
-write.table(export,row.names = FALSE,col.names = FALSE,sep = ",",
-            file = paste0(path_set,"/",filename))
-
-for(i in AllOtherRegions)
-{ 
-  # Get root_code of region 
-  reg_num <- i
-  reg_name <- as.character(root$region$Name[reg_num])
-  reg_num <- as.character(reg_num)
+  # Write RHS and SE data to folder
+  WriteFile(data_sel$RSE,paste0(filename$RHS,"RHS_",root$region$RootCountryAbbreviation[i],"_.csv")) 
+  WriteFile(data_sel$SE[i],paste0(filename$RHS,"SE_",root$region$RootCountryAbbreviation[i],"_.csv")) 
   
   # Add command for domestic Use table
-  ALANG <- add_row(ALANG,'1' = paste0("DataFeed IRP Extraction ",reg_name),
-                   Value = value,'Row parent' = reg_num,'Column parent' = reg_num,S.E. = SE)
+  ALANG <- add_row(ALANG,'#' = i)
 }
 
-################################################################################
+ALANG$`1` <- paste0("DataFeed IRP Extraction ",year," ",root$region$Name)
+ALANG$Value <- paste0(filename$RHS,"RHS_",root$region$RootCountryAbbreviation,"_.csv")                   
+ALANG$S.E. <- paste0(filename$RHS,"SE_",root$region$RootCountryAbbreviation,"_.csv")
 
-# Create industry concordance 
-max_ind <- length(root$industry$Code)
-Concord <-  matrix(0,nrow = 2, ncol = max_ind)
-Concord[1,1:4] <- 1
-Concord[2,5:max_ind] <- 1
+ALANG$Coef1 <- "1"
+ALANG$Years <- "1"
+ALANG$Margin <- "1"
 
-# Set name and path to concordance and write to folder
-Concorda_name <- "IRPextraction_Sec_Concordance"
-Concord_path <- paste0(path$Concordance,"/",Concorda_name,".csv")
-write.table(Concord,file = Concord_path,row.names = FALSE,col.names = FALSE,sep = ",")
-
-# Add path to concordance to ALANG commands
-ALANG$`Column grandchild` <- paste0("1:e a CONCPATH/",Concorda_name,".csv")
-
-# Add other variables for regions with data
+ALANG$`Row parent` <- "1:e"
 ALANG$`Row child` <- "3"
 ALANG$`Row grandchild` <- "1"
+
+ALANG$`Column parent` <- "1:e~3"
 ALANG$`Column child` <- "1"
-ALANG$`#` <- as.character(1:nrow(ALANG))
+ALANG$`Column grandchild` <- "1-4"
+
 ALANG$Incl <- "Y"
 ALANG$Parts <- "1"
 ALANG$`Pre-map` <- ""
 ALANG$`Post-map` <- ""
 ALANG$`Pre-Map` <- ""
 ALANG$`Post-Map` <- ""
-ALANG$Years <- "1"
-ALANG$Margin <- "1"
-ALANG$Coef1 <- "1"
   
 # Call script that writes the ALANG file to the repsective folder in the root
 source(paste0(path$root,"Rscripts/datafeeds_code/datafeed_subroutines/WriteALANG2Folder.R"))
