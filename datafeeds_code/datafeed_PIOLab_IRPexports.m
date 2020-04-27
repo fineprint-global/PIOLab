8a7787bd106392b2450da03da963da63f44dab9a
Hanspeter85 <hanspeter.wieland@wu.ac.at>
2020-04-27 17:58:06 +0200

    Restructured the folders in settings and concordances. Still have problems with one of the data feeds. Testing underway.

--- datafeeds_code/datafeed_PIOLab_IRPexports.m
+++
@@ -1,5 +0,0 @@
-function datafeed_PIOLab_IRPexports(handles)
-
-system('Rscript /import/emily1/isa/IELab/Roots/PIOLab/Rscripts/datafeeds_code/datafeed_PIOLab_IRPexports.R','-echo');
-
-end
--- datafeeds_code/datafeed_PIOLab_IRPimports.m
+++
@@ -1,5 +0,0 @@
-function datafeed_PIOLab_IRPimports(handles)
-
-system('Rscript /import/emily1/isa/IELab/Roots/PIOLab/Rscripts/datafeeds_code/datafeed_PIOLab_IRPimports.R','-echo');
-
-end
